94d8cf987e4c2556bddca0f4b64ca7052792caae
Aparecida <cidinha.dev@gmail.com>
2023-04-18 19:01:23 -0300

    ajustando o layout do Home

---
+++ src/sidebar/SideBar.jsx
@@ -0,0 +1,38 @@
+import "./sidebar.css"
+
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img src="https://images.pexels.com/photos/7497788/pexels-photo-7497788.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={250} height={200} alt="" />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic autem minima asperiores rerum nihil consectetur unde 
+        </p>
+      </div>
+
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">MÚSICA</li>
+          <li className="sidebarListItem">TEATRO</li>
+          <li className="sidebarListItem">CINEMA</li>
+          <li className="sidebarListItem">FILMES</li>
+          <li className="sidebarListItem">SÉRIES</li>
+        </ul>
+      </div>
+
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i class="sidebarIcons fa-brands fa-square-facebook"></i>
+          <i class="sidebarIcons fa-brands fa-square-pinterest"></i>
+          <i class="sidebarIcons fa-brands fa-square-twitter"></i>
+          <i class="sidebarIcons fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+
+    </div>
+  )
+}

dc159002f8ec455272b821439bebc02511cc64f2
Aparecida <cidinha.dev@gmail.com>
2023-04-24 19:54:11 -0300

    criando os posts

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -3,7 +3,7 @@ import "./post.css"
 export default function Post() {
   return (
     <div className="post">
-        <img src="" alt="" className="postImg" />
+        <img src="https://images.pexels.com/photos/3363111/pexels-photo-3363111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="postImg" />
         <div className="postInfo">
             <div className="postCats">
                 <span className="postCat">MÚSICA</span>
@@ -13,6 +13,7 @@ export default function Post() {
             <hr />
             <span className="postDate">1 hour ago</span>
         </div>
+        <p className="postDesc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia corrupti, vero a soluta ducimus velit sapiente molestiae quam aspernatur minima cumque provident atque error quas fuga repudiandae voluptas praesentium molestias.</p>
     </div>
   )
   
